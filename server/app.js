f9a4d38297f51cc2785ff11119a1edfb9467193c
Abdul Rahman <Abdulrahman.81869@gmail.com>
2025-02-19 10:34:24 +0000

    WebXperts

---
+++ server/app.js
@@ -0,0 +1,24 @@
+import express from "express"
+import config from "config"
+
+const app = express();
+const PORT = config.get("PORT") || 5044;
+
+app.use(express.json())
+
+app.get("/", (req, res)=>{
+    try {
+
+        res.status(200).json({msg: 'welcome to backend of WebXperts!'})
+        
+    } catch (error) {
+        res.status(401).json({msg: error})
+    }
+})
+
+app.listen(PORT, ()=> {
+    console.log(`your web app is running live at port ${PORT}`);
+    
+})
+
+
