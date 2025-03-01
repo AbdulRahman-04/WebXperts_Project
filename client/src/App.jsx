6cbe2ea1352fef510fdb6c0ed356d1d9676f1978
Abdul Rahman <Abdulrahman.81869@gmail.com>
2025-03-01 10:35:42 +0000

    WebXpertz

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Signup from "./Components/public/Signup"
+
+
+const App = () => {
+  return (
+    <>
+      
+      <Signup />
+
+
+    
+    </>
+  )
+}
+
+export default App
---
+++ client/src/Components/public/Signup.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Signup = () => {
+  return (
+    <> 
+      
+       <div className="bg-gra">
+
+
+        <div className="card border-2 bg-blue-800">
+
+        
+          
+          
+
+
+
+        </div>
+
+ 
+
+         
+      </div>
+
+    
+    </>
+  )
+}
+
+export default Signup
